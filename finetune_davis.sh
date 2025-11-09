cc7945f392a8e791bb4ad306010c2db39ab1fa70
vingumminr9 <oskarskanning@hotmail.com>
2025-11-09 06:07:16 +0100

    fix?

--- finetune_davis.sh
+++ finetune_davis.sh
@@ -7,4 +7,4 @@ echo "===== Starting Fine-Tune Experiments ====="
 python sam2/training/train.py \
     -c configs/sam2.1_training/sam2.1_hiera_b+_DAVIS_finetune.yaml \
     --use-cluster 0 \
-    --num-gpus 8
+    --num-gpus $(nvidia-smi -L | wc -l)
